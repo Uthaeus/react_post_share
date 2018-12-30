87491aa98826138d43dac6ce0eb40cfec418efb5
Uthaeus <romanlavery@gmail.com>
2018-12-29 23:32:57 -0700

    set up new project and created Post container

---
+++ src/containers/Post/Post.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+
+    render() {
+        return (
+            <p>Here is a post</p>
+        );
+    }
+}
+
+
+export default Post;
